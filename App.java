2e6f565c7356fc2e04a7818563dd34e1af37e2e9
Wesley Silva <wesley1234silva20@gmail.com>
2023-08-09 15:23:27 -0300

    meu primeiro commit

---
+++ App.java
@@ -0,0 +1,7 @@
+public class App{
+    public static void main(String args[]){
+    System.out.println("Meu Primeiro teste");
+    System.out.println("Meu Segundo teste");
+    System.out.println("Meu teste");
+    }
+}
